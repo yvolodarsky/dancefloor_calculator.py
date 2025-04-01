a10f90c196733779cac0937c6f649cf14c03ff46
yvolodarsky <yvolodarsky@me.com>
2025-04-01 11:47:15 -0600

    Update dancefloor_calculator.py

--- dancefloor_calculator.py
+++ dancefloor_calculator.py
@@ -12,7 +12,7 @@ length_ft = st.number_input("Dance Floor Length (ft)", min_value=1, value=20)
 # Constants
 panel_size_inch = 20
 panel_size_ft = panel_size_inch / 12  # Convert to feet
-cost_per_sqft = 10  # $10 per sqft
+cost_per_sqft = 15  # $15 per sqft
 
 # Calculate number of panels
 cols = math.ceil(width_ft / panel_size_ft)
